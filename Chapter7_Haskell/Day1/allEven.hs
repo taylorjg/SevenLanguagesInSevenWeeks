4039ef8248ecffdc3f91df386c6514533bbbda05
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-04-04 13:03:55 +0100

    Converted indentation to spaces

--- Chapter7_Haskell/Day1/allEven.hs
+++ Chapter7_Haskell/Day1/allEven.hs
@@ -13,17 +13,17 @@ allEven3 (x:xs) = isEven x && allEven3 xs
 
 testAllEven :: String -> ([Int] -> Bool) -> IO ()
 testAllEven n f =
-	do
-		test n f [1,2,3]
-		test n f [2,4,6]
-		where
-			test n f xs = 
-				putStrLn $ n ++ " " ++ show xs ++ " = " ++ (show $ f xs)
+    do
+        test n f [1,2,3]
+        test n f [2,4,6]
+        where
+            test n f xs = 
+                putStrLn $ n ++ " " ++ show xs ++ " = " ++ (show $ f xs)
 
 
 main :: IO ()
 main =
-	do
-		testAllEven "allEven1" allEven1
-		testAllEven "allEven2" allEven2
-		testAllEven "allEven3" allEven3
+    do
+        testAllEven "allEven1" allEven1
+        testAllEven "allEven2" allEven2
+        testAllEven "allEven3" allEven3
--- Chapter7_Haskell/Day1/listReverse.hs
+++ Chapter7_Haskell/Day1/listReverse.hs
@@ -6,14 +6,14 @@ listReverse2 :: [a] -> [a]
 listReverse2 xs = foldl (flip (:)) [] xs
 
 testListReverse n f =
-	do
-		test n f "abcdefg"
-		where
-			test n f xs = 
-				putStrLn $ n ++ " " ++ show xs ++ " = " ++ (show $ f xs)
+    do
+        test n f "abcdefg"
+        where
+            test n f xs = 
+                putStrLn $ n ++ " " ++ show xs ++ " = " ++ (show $ f xs)
 
 main :: IO ()
 main =
-	do
-		testListReverse "listReverse1" listReverse1
-		testListReverse "listReverse2" listReverse2
+    do
+        testListReverse "listReverse1" listReverse1
+        testListReverse "listReverse2" listReverse2
