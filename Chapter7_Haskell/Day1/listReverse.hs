1ccc3749d4a032c23b48da00b81cf294c2354b80
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-04-06 22:41:17 +0100

    Added another list reverse implementation

--- Chapter7_Haskell/Day1/listReverse.hs
+++ Chapter7_Haskell/Day1/listReverse.hs
@@ -5,6 +5,13 @@ listReverse1 (x:xs) = listReverse1 xs ++ [x]
 listReverse2 :: [a] -> [a]
 listReverse2 xs = foldl (flip (:)) [] xs
 
+listReverse3 :: [a] -> [a]
+listReverse3 xs = 
+    loop xs []
+    where
+        loop [] acc = acc
+        loop (x:xs) acc = loop xs (x:acc)
+
 testListReverse n f =
     do
         test n f "abcdefg"
@@ -17,3 +24,4 @@ main =
     do
         testListReverse "listReverse1" listReverse1
         testListReverse "listReverse2" listReverse2
+        testListReverse "listReverse3" listReverse3
