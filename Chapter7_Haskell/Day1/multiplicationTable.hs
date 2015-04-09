c7af5fa288de0c963c51886ed61b869f53a522aa
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-04-09 10:28:32 +0100

    Converted tabs to spaces

--- Chapter7_Haskell/Day1/multiplicationTable.hs
+++ Chapter7_Haskell/Day1/multiplicationTable.hs
@@ -8,12 +8,12 @@ group n xs = g:group n rest where (g,rest) = splitAt n xs
 
 main :: IO ()
 main = 
-	do
-		let n = 12
-		let tableTuples = multiplcationTable n
-		let formattedLines = map formatLine tableTuples
-		let tables = group n formattedLines
-		let tablesWithSeparators = intercalate [""] tables
-		mapM_ putStrLn tablesWithSeparators
-		where
-			formatLine (m1,m2,r) = show m1 ++ " x " ++ show m2 ++ " = " ++ show r
+    do
+        let n = 12
+        let tableTuples = multiplcationTable n
+        let formattedLines = map formatLine tableTuples
+        let tables = group n formattedLines
+        let tablesWithSeparators = intercalate [""] tables
+        mapM_ putStrLn tablesWithSeparators
+        where
+            formatLine (m1,m2,r) = show m1 ++ " x " ++ show m2 ++ " = " ++ show r
