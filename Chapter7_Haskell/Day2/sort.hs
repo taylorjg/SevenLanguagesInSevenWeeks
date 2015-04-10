c2890cb799941caf408a2d338e7c7152b3298f2f
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-04-10 22:01:59 +0100

    Converted tabs to spaces

--- Chapter7_Haskell/Day2/sort.hs
+++ Chapter7_Haskell/Day2/sort.hs
@@ -3,9 +3,9 @@ import Data.List (partition)
 sort :: Ord a => [a] -> [a]
 sort [] = []
 sort (x:xs) =
-	concat [sort as, return x, sort bs]
-	where
-		(as,bs) = partition (<x) xs
+    concat [sort as, return x, sort bs]
+    where
+        (as,bs) = partition (<x) xs
 
 main :: IO ()
 main = putStrLn $ show $ sort [3,2,1]
